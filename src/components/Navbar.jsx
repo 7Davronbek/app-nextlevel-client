094ba87766c6681b4fbf8aa391e7f8a2509ea0fd
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-03 15:58:01 +0500

    nextlevel-app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <>
-        <div className="navbar">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt fuga soluta dignissimos commodi at autem iusto repellendus, iure illum unde eius sint adipisci fugit nobis quaerat suscipit provident a ipsam!
+    return (
+        <>
+            <div className="navBar">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 d-flex aling-items-center">
+
+                            <h6>Stay Connected:</h6>
+
+                            <div className="socials">
+                                <span><img src="/assets/image/f.png" alt="" /></span>
+                                <span><img src="/assets/image/twit.png" alt="" /></span>
+                                <span><img src="/assets/image/ln.png" alt="" /></span>
+                                <span><img src="/assets/image/g.png" alt="" /></span>
+                            </div>
+
+                            <a href="!#">Advertise</a>
+                            <a href="!#">Support Forum </a>
+                            <a href="!#">Contact </a>
+
+                            <div className="ms-auto">
+                                <a href="!#"><span className="me-3"><img src="/assets/image/phone.png" alt="" /></span> 998 90 000 00 00</a>
+                                <a href="!#">English</a>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navbar
