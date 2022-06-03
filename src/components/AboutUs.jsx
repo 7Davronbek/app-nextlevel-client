db89d611755beda134a12f3b2a17cd7b7161a30a
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-03 19:33:27 +0500

    nextlevel-app

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <>
+        <div className="aboutUs">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem id dolore eum nulla ipsam sint aspernatur necessitatibus accusantium velit fugiat saepe quas non vel, eius provident tempora fugit dolor error.
+                    </div>
+                    <div className="col-lg-5">
+                        <h1>Next level</h1>
+                        <h2>Деловой Город </h2>
+                        <h3>Рекламно производственная компания</h3>
+                        <h4>Bears delivers bespoke public relations and marketing, underpinned by clever thinking, market insight, and a set of business ethics that ensure we get the best results for our clients. For us public relations is no dying art, it is a skill that’s very much alive alongside social, digital and traditional marketing.</h4>
+                        <h5>We are a team of passionate media relations professionals, writers, digital marketers and creative designers. Our clients are big and small, UK and international, operating in many different sectors.</h5>
+                        <h6>LEARN MORE</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default AboutUs
