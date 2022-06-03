30494ba8257d62afe8262749e294042557fe3ac2
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-03 11:45:17 +0500

    nextlevel-app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from 'react'
 const App = () => {
   return (
     <>
-    
+    <div className='vh-100'>
+      loem
+      loem
+      loem
+    </div>
+    <div className='vh-100'>
+      loem
+      loem
+      loem
+    </div>
     </>
   )
 }
