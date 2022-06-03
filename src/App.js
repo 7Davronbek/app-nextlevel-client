54eaf7082b8a3f96cd38c64a3730630d4a663120
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-03 18:41:05 +0500

    nextlevel-app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Menu />
     </>
   );
 };
---
+++ src/components/Menu.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <>
+            <div className="menu">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 d-flex align-items-center">
+                            <div className='d-flex align-items-center'>
+                                <div className="burger">
+                                    <span><img src="/assets/image/burger.png" alt="" /></span>
+                                    MENU
+                                </div>
+                                <div className="search"><img src="/assets/image/search.png" alt="" /></div>
+                            </div>
+                            <div className="logo"><img src="/assets/image/logo.png" alt="" /></div>
+                            <h6>SUBMIT YOUR AGENCY</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Menu
