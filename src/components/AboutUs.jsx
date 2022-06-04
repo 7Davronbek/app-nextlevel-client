f4fa8c9c495360298b48c9360e1c6be4f88ddf78
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-04 11:11:24 +0500

    nextlevel-app

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -9,13 +9,13 @@ const AboutUs = () => {
                     <div className="col-lg-5">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem id dolore eum nulla ipsam sint aspernatur necessitatibus accusantium velit fugiat saepe quas non vel, eius provident tempora fugit dolor error.
                     </div>
-                    <div className="col-lg-5">
+                    <div className="col-lg-5 text-center">
                         <h1>Next level</h1>
                         <h2>Деловой Город </h2>
                         <h3>Рекламно производственная компания</h3>
                         <h4>Bears delivers bespoke public relations and marketing, underpinned by clever thinking, market insight, and a set of business ethics that ensure we get the best results for our clients. For us public relations is no dying art, it is a skill that’s very much alive alongside social, digital and traditional marketing.</h4>
                         <h5>We are a team of passionate media relations professionals, writers, digital marketers and creative designers. Our clients are big and small, UK and international, operating in many different sectors.</h5>
-                        <h6>LEARN MORE</h6>
+                        <button className='btn  myBtn'>LEARN MORE</button>
                     </div>
                 </div>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                             <h1>Рекламно производственная компания </h1>
                             <h2>Looking for an Agency?</h2>
                             <h3>Explore the world’s leading local and global agencies to find the one that suits your needs.</h3>
-                            <button className="btn">Подробнее</button>
+                            <button className="btn myBtn">Подробнее</button>
 
                     </div>
                 </div>
