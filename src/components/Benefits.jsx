9aadd2d49ca5a780b6205ce2166f14f6ec3af818
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-04 12:31:32 +0500

    nextlevel-app

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,27 +1,64 @@
 import React from 'react'
 
 const Benefits = () => {
-  return (
-    <>
-        <div className="benefits">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-3 col-md-6">
-                        <div className="cards">
-                            <div className="left">
-                                <img src="/assets/image/icon1.png" alt="" />
-                            </div>
-                            <div className="right">
-                                <h2>3587</h2>
-                                <h3>Satisfied Clients</h3>
+    return (
+        <>
+            <div className="benefits">
+                <div className="container">
+                    <div className="row myRow">
+                        <div className="col-10 mx-auto">
+                            <div className="row">
+                                <div className="col-lg-3 col-md-6">
+                                    <div className="cards">
+                                        <div className="left">
+                                            <img src="/assets/image/icon1.png" alt="" />
+                                        </div>
+                                        <div className="right">
+                                            <h2>3587</h2>
+                                            <h3>Satisfied Clients</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6">
+                                    <div className="cards">
+                                        <div className="left">
+                                            <img src="/assets/image/icon2.png" alt="" />
+                                        </div>
+                                        <div className="right">
+                                            <h2>70</h2>
+                                            <h3>Employees</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6">
+                                    <div className="cards">
+                                        <div className="left">
+                                            <img src="/assets/image/icon3.png" alt="" />
+                                        </div>
+                                        <div className="right">
+                                            <h2>685</h2>
+                                            <h3>Resumes Posted</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6">
+                                    <div className="cards">
+                                        <div className="left">
+                                            <img src="/assets/image/icon4.png" alt="" />
+                                        </div>
+                                        <div className="right">
+                                            <h2>2093</h2>
+                                            <h3>Company Members</h3>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Benefits
