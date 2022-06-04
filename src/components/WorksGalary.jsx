f197ee0d461d72ec87ecdccedcabbde45edc8047
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-04 14:00:36 +0500

    nextlevel-app

--- src/components/WorksGalary.jsx
+++ src/components/WorksGalary.jsx
@@ -6,18 +6,65 @@ const WorksGalary = () => {
             <div className="worksGalary">
                 <div className="container">
                     <div className="row">
-                        <div className="col-12">
-                            <h1 className="h1">Наши работы</h1>
+                        <div className="col-12 text-center">
+                            <h1 className="h1">Наши работы <span>OUR WORKS</span></h1>
                             <h2>Best Agencies By Region</h2>
                         </div>
                     </div>
 
                     <div className="row">
-                        <div className="col-lg-4 col-md-6 mb-5">
-                            <img src="/assets/image/card.jpeg" alt="" className="w-100" />
-                            <div className="content">
-                                <h3>6S Marketing</h3>
-                                <h4>Europe</h4>
+                        <div className="col-lg-4 col-md-6 mb-4">
+                            <div className="cards">
+                                <img src="/assets/image/card.jpeg" alt="" className="w-100" />
+                                <div className="content">
+                                    <h3>6S Marketing</h3>
+                                    <h4>Europe</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4">
+                            <div className="cards">
+                                <img src="/assets/image/card.jpeg" alt="" className="w-100" />
+                                <div className="content">
+                                    <h3>6S Marketing</h3>
+                                    <h4>Europe</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4">
+                            <div className="cards">
+                                <img src="/assets/image/card.jpeg" alt="" className="w-100" />
+                                <div className="content">
+                                    <h3>6S Marketing</h3>
+                                    <h4>Europe</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4">
+                            <div className="cards">
+                                <img src="/assets/image/card.jpeg" alt="" className="w-100" />
+                                <div className="content">
+                                    <h3>6S Marketing</h3>
+                                    <h4>Europe</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4">
+                            <div className="cards">
+                                <img src="/assets/image/card.jpeg" alt="" className="w-100" />
+                                <div className="content">
+                                    <h3>6S Marketing</h3>
+                                    <h4>Europe</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4">
+                            <div className="cards">
+                                <img src="/assets/image/card.jpeg" alt="" className="w-100" />
+                                <div className="content">
+                                    <h3>6S Marketing</h3>
+                                    <h4>Europe</h4>
+                                </div>
                             </div>
                         </div>
                     </div>
