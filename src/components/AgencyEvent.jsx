f32d98089db6354f1447b4503f3da65324defa96
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-06 10:14:12 +0500

    nextlevel-app

---
+++ src/components/AgencyEvent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AgencyEvent = () => {
+    return (
+        <>
+            <div className="agencyEvent">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>Agency Recent Events</h1>
+                            <h2 className="fira">what’s happening in the digital world</h2>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AgencyEvent
