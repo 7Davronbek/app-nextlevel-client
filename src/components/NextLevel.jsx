e986d6d3ad30e6c9a3db489b63bfa0711818a9c9
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-08 11:55:55 +0500

    nextlevel-app

--- src/components/NextLevel.jsx
+++ src/components/NextLevel.jsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from 'react'
 const NextLevel = () => {
 
     const [offsetY, setOffsetY] = useState(0)
-    const handleScroll = () => setOffsetY(window.pageYOffset)
+    const handleScroll = () =>{
+        setOffsetY(window.pageYOffset)
+    }
 
     useEffect(() => {
         window.addEventListener("scroll", handleScroll)
@@ -13,15 +15,15 @@ const NextLevel = () => {
 
     return (
         <>
-            <div className="nextParralax">
+            <div style={{transform: 'translateX(-300px)'}} className="nextParralax">
                 <h1 style={{ transform: `translateX( ${offsetY * 0.2}px)`,  left: "-150px" }}>Next Level</h1>
                 <h1 style={{ transform: `translateX( ${offsetY * 0.2}px)` }}>Next Level</h1>
                 <h1 style={{ transform: `translateX( ${offsetY * 0.2}px)`,  right: "-150px" }}>Next Level</h1>
             </div>
-            <div className="nextParralax">
+            <div style={{transform: 'translateX(400px)'}} className="nextParralax pt-5">
+                <h1 style={{ transform: `translateX( ${offsetY * -0.2}px)`, left: "-150px" }}>Next Level</h1>
                 <h1 style={{ transform: `translateX( ${offsetY * -0.2}px)` }}>Next Level</h1>
-                <h1 style={{ transform: `translateX( ${offsetY * -0.2}px)` }}>Next Level</h1>
-                <h1 style={{ transform: `translateX( ${offsetY * -0.2}px)` }}>Next Level</h1>
+                <h1 style={{ transform: `translateX( ${offsetY * -0.2}px)`, right: "-150px" }}>Next Level</h1>
             </div>
         </>
     )
