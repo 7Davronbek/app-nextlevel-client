ff7512640014929c983554ddf9b7bde01e88e9aa
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-09 14:56:25 +0500

    practise

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -8,7 +8,6 @@ const GridLayout = () => {
                     <div className="row">
                         <div className="col-12">
                             {/* <div className="gallery">
-
                                 <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
                                 <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
                                 <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
@@ -26,84 +25,20 @@ const GridLayout = () => {
                                 <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
                                 <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
                                 <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-
                             </div> */}
 
                             <div className="image-gallary">
                                 <div className="image-box">
                                     <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <h6>Lorem, ipsum dolor.</h6>
                                 </div>
                                 <div className="image-box">
                                     <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <h6>Lorem, ipsum dolor.</h6>
                                 </div>
                                 <div className="image-box">
                                     <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
-                                </div>
-                                <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <h6>Lorem, ipsum dolor.</h6>
                                 </div>
                             </div>
                         </div>
