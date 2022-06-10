b283d74b1fd17ec969b9f0a0e1ed658de17ed4a0
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-10 11:49:28 +0500

    nextlevel-app

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -7,88 +7,88 @@ const GridLayout = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-12 mb-5 text-center">
-                            <h1>Спектр производимой продукции и предоставляемых услуг:</h1>
+                            <h1 className='boldta'>Спектр производимой продукции и предоставляемых услуг:</h1>
                         </div>
                         <div className="col-11 mx-auto text-center">
                             {/* <div className="gallery">
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
-                                <div className="gallery-item"><img src="/assets/image/card.jpeg" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
+                                <div className="gallery-item"><img src="/assets/image/port" alt="" /></div>
                             </div> */}
 
                             <div className="image-gallary">
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Торговое оборудование</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port2.jpg`} alt="" />
                                     <h6 className="fira">POSM</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port3.webp`} alt="" />
                                     <h6 className="fira">Офисная мебель</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port1.jpg`} alt="" />
                                     <h6 className="fira">Вывески</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port5.jpg`} alt="" />
                                     <h6 className="fira">Интерьерное оформление</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Световые короба</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Изготовление и застройка выставочных стендов</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Крышные конструкции</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Стелы, пилоны, указатели, навигационные материалы</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Крышные конструкции</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Стелы, пиловы, указатели, указатели, навигационные материалы</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">основанные комплексы</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Маркетирование</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Архитектурная подсветка</h6>
                                 </div>
                                 <div className="image-box">
-                                    <img src={`/assets/image/card.jpeg`} alt="" />
+                                    <img src={`/assets/image/port`} alt="" />
                                     <h6 className="fira">Сувенирная продукция</h6>
                                 </div>
                             </div>
