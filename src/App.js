5f789b5127d73106004e4b2b4d2eee0f7ee261b6
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-04 12:22:24 +0500

    nextlevel-app

--- src/App.js
+++ src/App.js
@@ -18,48 +18,48 @@ import Menu from "./components/Menu";
 // import Loader from "./components/Loader";
 
 const App = () => {
-  const [loader, setLoader] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoader(false);
-    }, 3000);
-  }, []);
-  // window.addEventListener("load", () => {
-  //   setLoader(false)
-  // })
-  return (
-    <>
-      {/* {loader ? (
+    const [loader, setLoader] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoader(false);
+        }, 3000);
+    }, []);
+    // window.addEventListener("load", () => {
+    //   setLoader(false)
+    // })
+    return (
+        <>
+            {/* {loader ? (
         <>
           <Loader />
         </>
       ) : (
         ""
       )} */}
-      <>
-        <Menu />
-        <Header />
-        <div className="myWrap">
-          <div className="bg1"><img src="/assets/image/bgLeft.png" alt="" /></div>
-          <div className="bg2"><img src="/assets/image/leftBottom.png" alt="" /></div>
-          <div className="bg3"><img src="/assets/image/rightTop.png" alt="" /></div>
-          <div className="bg4"><img src="/assets/image/rightBottom.png" alt="" /></div>
-          <AboutUs />
-          <GridLayout />
-        </div>
-        <Benefits />
-        {/* <NextLevel /> */}
-        {/* <WorksGalary /> */}
-        {/* <SumbitAgency /> */}
-        {/* <LookingFor /> */}
-        {/* <AgencyEvent /> */}
-        {/* <Portfolio /> */}
-        {/* <Partners /> */}
-        <Forms />
-        <Footer />
-      </>
-    </>
-  );
+            <>
+                <Menu />
+                <Header />
+                <div className="myWrap">
+                    <div className="bg1"><img src="/assets/image/bgLeft.png" alt="" /></div>
+                    <div className="bg2"><img src="/assets/image/leftBottom.png" alt="" /></div>
+                    <div className="bg3"><img src="/assets/image/rightTop.png" alt="" /></div>
+                    <div className="bg4"><img src="/assets/image/rightBottom.png" alt="" /></div>
+                    <AboutUs />
+                    <GridLayout />
+                </div>
+                <Benefits />
+                {/* <NextLevel /> */}
+                {/* <WorksGalary /> */}
+                {/* <SumbitAgency /> */}
+                {/* <LookingFor /> */}
+                {/* <AgencyEvent /> */}
+                {/* <Portfolio /> */}
+                {/* <Partners /> */}
+                <Forms />
+                <Footer />
+            </>
+        </>
+    );
 };
 
 export default App;
