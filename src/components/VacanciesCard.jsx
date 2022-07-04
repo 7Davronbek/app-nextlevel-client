1fbc0667f7126554396a68bf39d8717ff574c42e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-04 17:58:37 +0500

    nextlevel-app

---
+++ src/components/VacanciesCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const VacanciesCard = () => {
+  return (
+    <div className='VacanciesCard'>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default VacanciesCard
